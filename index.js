f787c64764551d303fe4bf2a6b090424febc4516
Shangyi Chen <shangyichen@shangyis-air.lan>
2021-01-03 22:42:56 -0800

    2nd

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
-
 app.listen(4000,()=> console.log('listening to 4000'));
 app.use(express.static('public'));
+app.use(express.json({limit:'1mb'}));
+
+app.post('/api',(request,response)=>{
+  console.log(request.body);
+  const data = request.body;
+  response.json({
+  	status:'success',
+  	latitude:data.lat,
+  	longitude:data.lon
+  });
+})
