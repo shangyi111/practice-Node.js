5692184400e8a1cffbd83942348edb0cf3e8f8d7
Shangyi Chen <shangyichen@shangyis-air.lan>
2021-01-04 22:33:29 -0800

    3rd update

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require('express');
+const Datastore = require('nedb');
+
 const app = express();
 app.listen(4000,()=> console.log('listening to 4000'));
 app.use(express.static('public'));
 app.use(express.json({limit:'1mb'}));
 
-app.post('/api',(request,response)=>{
-  console.log(request.body);
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+app.get('/api',(request,response)=>{
+	database.find({},(err,data)=>{
+		if(err){
+			response.end();
+			return;
+		}
+		response.json(data);	
+	});
+})
+app.post('/something',(request,response)=>{
+  //console.log(request.body);
+  //console.log('Receiving from client, and then I will sending back my response to client');
   const data = request.body;
-  response.json({
-  	status:'success',
-  	latitude:data.lat,
-  	longitude:data.lon
-  });
+  const timestamp= Date.now();
+  data.timestamp = timestamp;
+  database.insert(data);
+  response.json(data);
 })
